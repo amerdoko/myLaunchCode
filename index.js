cd10af628370d000715021d8dcf012e6bf1890f6
Amer Doko <amerdoko@gmail.com>
2020-04-19 19:01:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// console.log(typeof "17");
+// console.log(typeof "3.2");
+// console.log("string's string");
+// console.log('Bruce\'s beard');
+// console.log(Number('3 3'));
+// let str = `Hi!`;
+// let phrase = `can embed another ${str}`;
+// console.log(phrase + " " + str);
+
+	// let name = "Jane Doe";
+	// console.log(typeof name);
+// let day = "Thursday";
+//  day = 32.5;
+//  day = 19;
+//  console.log(day);
+//console.log(2 ** 2 ** 3 * 3);
+// console.log(Boolean("true"));
+// console.log(Boolean("TRUE"));
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean(''));
+// console.log(Boolean('Launch Code'));
+
+//3 == 4;
+//3 + 4;
+//3 + 4 === 7;
+//"false"
+4 < 3 || 2 < 3
